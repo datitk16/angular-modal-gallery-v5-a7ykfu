7c66676fdc0c705de50ffcf5a4aba81f163cd3cb
devcodedao <datndd2604@gmail.com>
2020-06-27 20:18:36 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-modal-gallery-v5-a7ykfu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-modal-gallery-v5-a7ykfu)
---
+++ app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+// ********************** angular-modal-gallery *****************************
+import 'hammerjs'; // <------ mandatory dependency for angular-modal-gallery
+import 'mousetrap'; // <------ mandatory dependency for angular-modal-gallery
+import { ModalGalleryModule } from 'angular-modal-gallery'; // <----------------- angular-modal-gallery library import
+// **************************************************************************
+
+// ************************ optional font-awesome 5 ************************
+// to install use both `npm i --save @fortawesome/fontawesome` and `npm i --save @fortawesome/fontawesome-free-solid`
+import { faExternalLinkAlt, faPlus, faTimes, faDownload } from '@fortawesome/fontawesome-free-solid';
+import * as fontawesome from '@fortawesome/fontawesome';
+fontawesome.library.add(faExternalLinkAlt, faPlus, faTimes, faDownload);
+// *************************************************************************
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    ModalGalleryModule.forRoot() // <----------------- angular-modal-gallery module import
+  ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
